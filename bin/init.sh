a4e856d1d9f10651e85968a812339dc13afe0a14
artem <mos_artem2@mail.ru>
2017-11-29 21:18:49 +0300

    init .env

--- bin/init.sh
+++ bin/init.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+cp $(dirname $0)/../.env.dist $(dirname $0)/../.env
 cp $(dirname $0)/../payment.env.example $(dirname $0)/../payment.env
 cp $(dirname $0)/../workspace/.env.example $(dirname $0)/../workspace/.env
